99bbdd3d97a5b97d4420a4b54a69c451fac4c45d
veepee <66334129+vpontual@users.noreply.github.com>
2024-03-19 11:09:22 -0400

    used placeholder variables to fetch all desired api values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 console.log("Hello World");
 
+const cuisineType = "American";
+const mealType = "Lunch";
+const dishType = "Sandwiches";
+const time = "20";
+const excluded = "gluten";
+
 function fetchTimedRecipes() {
-  const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=05e0e399&app_key=eac8bc585be359a5c08825b3eccf7a8f&time=30`;
+  const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=05e0e399&app_key=eac8bc585be359a5c08825b3eccf7a8f&cuisineType=${cuisineType}&mealType=${mealType}&dishType=${dishType}&time=${time}&excluded=${excluded}`;
 
   fetch(apiUrl)
     .then((response) => response.json())
