b170580259ac1ba3e335891c14576fed3d42e80e
veepee <66334129+vpontual@users.noreply.github.com>
2024-03-19 11:08:58 -0400

    added api parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 console.log("Hello World");
 
-function fetchTimedRecipes(time) {
+function fetchTimedRecipes() {
   const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=05e0e399&app_key=eac8bc585be359a5c08825b3eccf7a8f&time=30`;
 
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      displayTimedRecipes(data);
+      console.log(data);
+      // displayTimedRecipes(data);
     })
     .catch((error) => {
       console.error("Error:", error);
@@ -14,5 +15,7 @@ function fetchTimedRecipes(time) {
       forecast.innerHTML = "";
     });
 }
+
+fetchTimedRecipes();
 
 function displayTimedRecipes() {}
