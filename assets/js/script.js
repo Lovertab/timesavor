2cff79016eeca1a7e4910bd9ea500481ce4b008c
veepee <66334129+vpontual@users.noreply.github.com>
2024-03-19 12:25:50 -0400

    added DOM elements for the API variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+///API Key
+const appID = "05e0e399";
+const appKey = "eac8bc585be359a5c08825b3eccf7a8f";
+/// DOM element references
+// const cuisineType = document.getElementById("cuisineSelect");
+// const mealType = document.getElementById("mealType");
+// const dishType = document.getElementById("dishType");
+// const time = document.getElementById("timeDuration");
+// const excluded = document.getElementById("excludeOption");
+
 console.log("Hello World");
 
 const cuisineType = "American";
@@ -7,7 +17,7 @@ const time = "20";
 const excluded = "gluten";
 
 function fetchTimedRecipes() {
-  const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=05e0e399&app_key=eac8bc585be359a5c08825b3eccf7a8f&cuisineType=${cuisineType}&mealType=${mealType}&dishType=${dishType}&time=${time}&excluded=${excluded}`;
+  const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${appID}&app_key=${appKey}&cuisineType=${cuisineType}&mealType=${mealType}&dishType=${dishType}&time=${time}&excluded=${excluded}`;
 
   fetch(apiUrl)
     .then((response) => response.json())
@@ -24,4 +34,9 @@ function fetchTimedRecipes() {
 
 fetchTimedRecipes();
 
-function displayTimedRecipes() {}
+function displayTimedRecipes() {
+  const { hits } = data.list[0];
+  const { description, icon } = weather[0];
+  const { temp, humidity } = main;
+  const { speed } = wind;
+}
