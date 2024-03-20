c9387cdcdb0d87309ac864ef77feb83d49e253ce
veepee <66334129+vpontual@users.noreply.github.com>
2024-03-20 18:30:34 -0400

    added time element to the recipe detail

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,56 +1,3 @@
-//.btn-primary
-// Get the recipe container element
-/*
-const recipeContainer = document.getElementById("recipe-container");
-console.log("HERE" + recipeContainer);
-function createRecipeContainer(recipe) {
-  // Create the container element
-  const recipeContainerEl = document.createElement("div");
-  recipeContainerEl.classList.add("recipe-container");
-  // Create the recipe image element
-  const recipeImg = document.createElement("img");
-  recipeImg.classList.add("recipe-img");
-  recipeImg.src = recipe.image;
-  recipeContainerEl.appendChild(recipeImg);
-  // Create the recipe name element
-  const recipeName = document.createElement("h2");
-  recipeName.classList.add("recipe-name");
-  recipeName.innerText = recipe.label;
-  recipeContainerEl.appendChild(recipeName);
-  // Create the recipe ingredients element
-  const recipeIngredients = document.createElement("ul");
-  recipeIngredients.classList.add("recipe-ingredients");
-  recipe.ingredients.forEach((ingredient) => {
-    const ingredientItem = document.createElement("li");
-    ingredientItem.innerText = ingredient.text;
-    recipeIngredients.appendChild(ingredientItem);
-  });
-  recipeContainerEl.appendChild(recipeIngredients);
-  // Create the recipe instructions element
-  const recipeInstructions = document.createElement("a");
-  recipeInstructions.classList.add("recipe-instructions");
-  recipeInstructions.href = recipe.url;
-  recipeInstructions.innerText = "View Recipe Instructions";
-  recipeContainerEl.appendChild(recipeInstructions);
-  // Return the container element
-  return recipeContainerEl;
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  const urlParameters = new URLSearchParams(window.location.search);
-  const recipeParam = urlParameters.get("recipe");
-
-  if (recipeParam) {
-    const recipe = JSON.parse(recipeParam);
-    const recipeContainer = createRecipeContainer(recipe);
-    const recipeContainerDiv = document.getElementById("recipe-container");
-    recipeContainerDiv.appendChild(recipeContainer);
-  } else {
-    console.error("Recipe data not found");
-  }
-});
-*/
-
 document.addEventListener("DOMContentLoaded", function () {
   const recipeData = sessionStorage.getItem("recipeData");
 
@@ -80,17 +27,21 @@ function createRecipeContainer(recipe) {
   leftCol.classList.add("col-md-8");
   row.appendChild(leftCol);
 
-  // Create the recipe name element
+  // Create the recipe title element
   const recipeName = document.createElement("h2");
   recipeName.classList.add("recipe-name");
   recipeName.innerText = recipe.label;
-  recipeName.style.borderBottom = "2px solid #000";
-  recipeName.style.paddingBottom = "10px";
   leftCol.appendChild(recipeName);
+
+  // Create the cooking time element
+  const cookingTime = document.createElement("p");
+  cookingTime.classList.add("cooking-time");
+  cookingTime.innerText = `Total Cooking Time: ${recipe.totalTime} minutes`;
+  leftCol.appendChild(cookingTime);
 
   // Create the recipe ingredients element
   const recipeIngredients = document.createElement("ul");
-  recipeIngredients.classList.add("recipe-ingredients");
+  recipeIngredients.classList.add("recipe-ingredients", "mt-3"); // Add mt-3 class for top margin
   recipe.ingredients.forEach((ingredient) => {
     const ingredientItem = document.createElement("li");
     ingredientItem.innerText = ingredient.text;
